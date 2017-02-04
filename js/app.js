622cd3e0893908e8972982215d2368cb10255e6b
taasmoe <tobias.aasmoe@gmail.com>
2017-02-04 14:08:54 +0100

    lagt til alle stasjoner på kart

--- js/app.js
+++ js/app.js
@@ -4,15 +4,11 @@ $(function () {
 
         console.log(json["stations"]);
 
+        /*
         var lat = json["stations"][1]["center"]["latitude"];
         var long = json["stations"][1]["center"]["longitude"];
         var title = json["stations"][1]["title"];
-
-        console.log(title);
-
-        var latlongArr = [lat, long];
-
-        console.log(latlongArr);
+        */
 
         var map = L.map('map').setView([59.930, 10.757933], 12);
 
@@ -20,6 +16,14 @@ $(function () {
             attribution: '<a href="http://www.kartverket.no/">Kartverket</a>'
         }).addTo(map);
 
-        L.marker(latlongArr, title).addTo(map);
+
+        for (var i = 0; i < json["stations"].length; i++) {
+            var lat = json["stations"][i]["center"]["latitude"];
+            var long = json["stations"][i]["center"]["longitude"];
+            var latlongArr = [lat, long];
+            var title = json["stations"][i]["title"];
+
+            L.marker(latlongArr, [title]).addTo(map);
+        }
     });
 });
