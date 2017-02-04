110031a6800c6cd6e6da63ef6ad82d4d5b17413c
Stigjb <stigjb@gmail.com>
2017-02-04 14:46:54 +0100

    Røde streker mellom stativer

--- js/app.js
+++ js/app.js
@@ -1,21 +1,12 @@
 $(function () {
-
     $.getJSON("stations.json", function (json) {
-
         console.log(json["stations"]);
-
-        /*
-        var lat = json["stations"][1]["center"]["latitude"];
-        var long = json["stations"][1]["center"]["longitude"];
-        var title = json["stations"][1]["title"];
-        */
 
         var map = L.map('map').setView([59.930, 10.757933], 12);
 
         L.tileLayer('http://opencache.statkart.no/gatekeeper/gk/gk.open_gmaps?layers=norges_grunnkart_graatone&zoom={z}&x={x}&y={y}', {
             attribution: '<a href="http://www.kartverket.no/">Kartverket</a>'
         }).addTo(map);
-
 
         for (var i = 0; i < json["stations"].length; i++) {
             var lat = json["stations"][i]["center"]["latitude"];
@@ -24,6 +15,17 @@ $(function () {
             var title = json["stations"][i]["title"];
 
             L.marker(latlongArr, [title]).addTo(map);
+        }
+
+        for (var i = 1; i < json["stations"].length; i++) {
+            var latlngs = [
+                [json["stations"][i - 1]["center"]["latitude"],
+                    json["stations"][i - 1]["center"]["longitude"]],
+                [json["stations"][i]["center"]["latitude"],
+                    json["stations"][i]["center"]["longitude"]]
+            ];
+
+            L.polyline(latlngs, {color: "red"}).addTo(map);
         }
     });
 });
