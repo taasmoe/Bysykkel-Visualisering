de29f4027b82ef1196eacf964ff8f03b2c781245
Stigjb <stigjb@gmail.com>
2017-02-04 14:48:19 +0100

    glemte å fikse mergekonflikt i app.jl

--- js/app.js
+++ js/app.js
@@ -2,15 +2,11 @@ $(function () {
     $.getJSON("stations.json", function (json) {
         console.log(json["stations"]);
 
-<<<<<<< HEAD
-        var map = L.map('map').setView([59.930, 10.757933], 12);
-=======
         var map = L.map('map', {
             center: [59.930, 10.747933],
             zoom: 12.5,
             minZoom: 12.5
         });
->>>>>>> 26e4deacfeee664bd490e96872b19849bb50adf2
 
         L.tileLayer('http://opencache.statkart.no/gatekeeper/gk/gk.open_gmaps?layers=norges_grunnkart_graatone&zoom={z}&x={x}&y={y}', {
             attribution: '<a href="http://www.kartverket.no/">Kartverket</a>'
