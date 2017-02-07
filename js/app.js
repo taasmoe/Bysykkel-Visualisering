6eae5b8a4444350373dab6a0ea6c247ca9cf33e1
taasmoe <tobias.aasmoe@gmail.com>
2017-02-07 18:45:29 +0100

    endret slik at JSON-data er "globalt" tilgjengelig. Letter litt på avhengighetene $.getJSON skapte.

--- js/app.js
+++ js/app.js
@@ -1,68 +1,80 @@
 $(function () {
 
     // TODO Se om vi klarer fikse dette.
-    /*
-    var data = $.getJSON("stations.json", function (json) {
-        return json["responseJSON"];
-    });
-    */
 
-    // console.log(data);
-
-    $.getJSON("stations.json", function (json) {
-
-        var map = L.map('map', {
-            center: [59.930, 10.747933],
-            zoom: 12.5,
-            minZoom: 12.5
+    function getData(callback){
+        $.getJSON( "stations.json", function(data) {
+            callback(data);
         });
+    }
 
-        L.tileLayer('http://opencache.statkart.no/gatekeeper/gk/gk.open_gmaps?layers=norges_grunnkart_graatone&zoom={z}&x={x}&y={y}', {
-            attribution: '<a href="http://www.kartverket.no/">Kartverket</a>'
-        }).addTo(map);
+    getData(function(json){
+
+    });
+
+    var map = L.map('map', {
+        center: [59.930, 10.72933],
+        zoom: 13,
+        minZoom: 13
+    });
 
 
-        var markers = [];
-        var markersLayer;
+    L.tileLayer('http://opencache.statkart.no/gatekeeper/gk/gk.open_gmaps?layers=norges_grunnkart_graatone&zoom={z}&x={x}&y={y}', {
+        attribution: '<a href="http://www.kartverket.no/">Kartverket</a>'
+    }).addTo(map);
 
-        function leggTilMarkers() {
+    var markers = [];
+    var markersLayer;
+
+
+    function leggTilMarkers() {
+
+        getData(function(json){
 
             for (var i = 0; i < json["stations"].length; i++) {
+
                 var lat = json["stations"][i]["center"]["latitude"];
                 var long = json["stations"][i]["center"]["longitude"];
-
-                var mark = L.marker([lat, long], {riseOnHover: true})
+                var mark = new L.marker([lat, long], {riseOnHover: true})
                     .bindPopup(json["stations"][i]["title"]);
 
                 markers.push(mark);
             }
 
             markersLayer = L.layerGroup(markers).addTo(map);
-            console.log(markers.length);
-        }
 
-
-        $("#visStativer").click(function () {
-
-            leggTilMarkers();
-
-            $("#visStativer").attr("disabled", true);
-        });
-
-
-        $("#skjulStativer").click(function () {
-
+            /*
             markersLayer.eachLayer(function (layer) {
-                markersLayer.removeLayer(layer);
+                layer.on('click', onClick());
             });
+            */
 
-            markers = [];
+        });
+    }
 
-            $("#visStativer").attr("disabled", false);
+    $("#visStativer").click(function () {
+
+        leggTilMarkers();
+
+        $("#visStativer").attr("disabled", true);
+    });
+
+
+    $("#skjulStativer").click(function () {
+
+        markersLayer.eachLayer(function (layer) {
+            markersLayer.removeLayer(layer);
         });
 
+        markers = [];
 
-        $("#visStier").click(function () {
+        $("#visStativer").attr("disabled", false);
+    });
+
+
+    $("#visStier").click(function () {
+
+        getData(function(json){
             for (var i = 1; i < json["stations"].length; i++) {
                 var latlngs = [
                     [json["stations"][i - 1]["center"]["latitude"],
@@ -74,28 +86,31 @@ $(function () {
                 L.polyline(latlngs, {color: "coral"}).addTo(map);
             }
         });
+    });
 
 
-        $("#skjulStier").click(function () {
-            fjernAlleStier();
-        });
+    $("#skjulStier").click(function () {
+        fjernAlleStier();
+    });
 
 
-        function fjernAlleStier() {
-            for(i in map._layers) {
-                if(map._layers[i]._path != undefined) {
-                    try {
-                        map.removeLayer(map._layers[i]);
-                    }
-                    catch(e) {
-                        console.log("problem with " + e + map._layers[i]);
-                    }
+    function fjernAlleStier() {
+        for(i in map._layers) {
+            if(map._layers[i]._path != undefined) {
+                try {
+                    map.removeLayer(map._layers[i]);
+                }
+                catch(e) {
+                    console.log("problem with " + e + map._layers[i]);
                 }
             }
         }
+    }
 
 
-        $("#legg-til-sti").click(function () {
+    $("#legg-til-sti").click(function () {
+
+        getData(function(json){
             var id1 = $("#id1").val();
             var id2 = $("#id2").val();
 
@@ -111,11 +126,13 @@ $(function () {
 
             L.polyline(latlngs, {color: "red"}).addTo(map);
         });
+    });
 
 
-        $("#legg-til-tre").click(function () {
+    $("#legg-til-tre").click(function () {
+
+        getData(function(data){
             var edgeData = tree;
-
             var edges = [];
 
             for (var j = 0; j < edgeData.length; j++) {
